167db60d061409c51d4e7c40182c7e43dd2c3aa4
oosuhada <woosu91@gmail.com>
2025-01-12 20:08:27 +0900

    feat(app): implement database-interface screen logic

--- lib/core/services/database_interface.dart
+++
@@ -1,8 +0,0 @@
-// database_interface.dart
-import 'dart:async';
-
-abstract class DatabaseInterface {
-  Future<void> initialize();
-  Future<int> insertRecord(Map<String, dynamic> record);
-  Future<void> close();
-}
