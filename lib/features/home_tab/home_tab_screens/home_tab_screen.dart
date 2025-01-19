9f3ea04eb622c689f413fd0ba1e04bce525f0130
oosuhada <woosu91@gmail.com>
2025-01-19 10:40:44 +0900

    feat(app): implement home-tab-screen screen logic

--- lib/features/home_tab/home_tab_screens/home_tab_screen.dart
+++ lib/features/home_tab/home_tab_screens/home_tab_screen.dart
@@ -1,6 +1,7 @@
 // home_tab_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_onjung_v1/%08shared/widgets/bottom_navigation_bar.dart';
+import 'package:flutter_onjung_v1/core/config/app_router.dart';
 import 'package:flutter_onjung_v1/data/home_tab/statistics_models.dart'
     as statistics_model;
 import 'package:flutter_onjung_v1/features/home_tab/home_tab_screens/home_tabbed_screen.dart';
@@ -95,16 +96,16 @@ class _HomeTabScreenState extends State<HomeTabScreen> {
         onTap: (index) {
           switch (index) {
             case 0:
-              context.go('/home');
+              context.goNamed(AppRoute.homeTab.name);
               break;
             case 1:
-              context.go('/address');
+              context.goNamed(AppRoute.addressTab.name);
               break;
             case 2:
-              context.go('/calendar');
+              context.goNamed(AppRoute.calendarTab.name);
               break;
             case 3:
-              context.go('/myOnjung');
+              context.goNamed(AppRoute.onjungTab.name);
               break;
           }
         },
