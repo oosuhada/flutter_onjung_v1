d97896d2bb15928e252b2c521d4231467a6b49aa
oosuhada <woosu91@gmail.com>
2025-02-03 14:24:37 +0900

    feat(app): implement calendar-tab-screen screen logic

--- lib/features/calendar_tab/screens/calendar_tab_screen.dart
+++ lib/features/calendar_tab/screens/calendar_tab_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_onjung_v1/%08shared/widgets/bottom_navigation_bar.dart';
-import 'package:flutter_onjung_v1/features/calendar_tab/widgets/drawer_menu.dart';
+import 'package:flutter_onjung_v1/%08shared/widgets/drawer_menu.dart';
+import 'package:flutter_onjung_v1/core/config/app_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -98,16 +99,16 @@ class _CalendarTabScreenState extends ConsumerState<CalendarTabScreen>
         onTap: (index) {
           switch (index) {
             case 0:
-              context.go('/home');
+              context.goNamed(AppRoute.homeTab.name);
               break;
             case 1:
-              context.go('/address');
+              context.goNamed(AppRoute.addressTab.name);
               break;
             case 2:
-              context.go('/calendar');
+              context.goNamed(AppRoute.calendarTab.name);
               break;
             case 3:
-              context.go('/myOnjung');
+              context.goNamed(AppRoute.onjungTab.name);
               break;
           }
         },
